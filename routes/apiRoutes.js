c011d2ebbc1e0f49c7d2de0be828c23966561fa7
Brandi Murray <pritchettb14@gmail.com>
2021-04-21 21:40:13 -0400

    all functionality working can create new notes and delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,26 @@
+let db = require("../db/db.json")
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+
 module.exports = (app) => {
 
     app.get("/api/notes", (req, res) => {
+         res.json(db)
+    })
+    app.post("/api/notes", (req, res) =>{
+        req.body.id = uuidv4()
+        db.push(req.body)
+        fs.writeFileSync("./db/db.json", JSON.stringify(db))
 
+        res.json(db)
+    })
+
+    app.delete("/api/notes/:id", (req, res) => {
+
+       db = db.filter(note=>note.id != req.params.id)
+       fs.writeFileSync("./db/db.json", JSON.stringify(db))
+
+
+       res.json(db)
     })
 }
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
+app.use(express.static('public')); // makes client side public in browser - important 
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
